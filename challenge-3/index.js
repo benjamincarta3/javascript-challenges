64233922451ddbb793b24e2d51d2b1d8489ac6ef
benjamincarta3 <benjamincarta3@gmail.com>
2026-02-22 00:40:05 +0000

    add challenge-3

---
+++ challenge-3/index.js
@@ -0,0 +1,16 @@
+// Print the multiplication table with 7 //
+
+// Using for loop //
+
+for (let i = 1; i <= 10; i++) {
+    console.log("7 x " + i + " = " + (7 * i));
+}
+
+// Using while loop //
+
+let b = 1;
+
+while (b <= 10) {
+    console.log("7 x " + b + " = " + (7 * b));
+    b++;
+}
